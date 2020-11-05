c8f58b72841d7c1519ff8725a55f623a360c8aeb
yayguy4618 <68721523+yayguy4618@users.noreply.github.com>
2020-11-06 01:56:52 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 pip3 install -r requirements.txt
-mkdir ~/.local/share/homeaccountant
-cp db.db ~/.local/share/homeaccountant
+cp db.db ~/.local/bin
 cp main.py ~/.local/bin
 cd ~/.local/bin
 mv main.py homeaccountant
