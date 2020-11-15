4d14443fa7cff3abba721eb663ec50f0526053e7
yayguy4618 <68721523+yayguy4618@users.noreply.github.com>
2020-11-16 01:42:54 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@ sudo pip3 uninstall pyqt5
 pip3 install -r requirements.txt
 mkdir $HOME/.homeaccountant/
 cp db.db $HOME/.homeaccountant/
-cp main.py /usr/bin
+sudo cp main.py /usr/bin
 cd /usr/bin
-mv main.py homeaccountant
-chmod +x homeaccountant
+sudo mv main.py homeaccountant
+sudo chmod +x homeaccountant
 
