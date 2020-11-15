46944f5a2f1cf90599cfa56d6ff7a312fb3d8059
yayguy4618 <68721523+yayguy4618@users.noreply.github.com>
2020-11-15 23:04:54 +0900

    Delete install.sh

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-pip3 install -r requirements.txt
-mkdir /usr/share/homeaccountant/
-cp db.db /usr/share/homeaccountant/
-cp main.py /usr/bin
-cd /usr/bin
-mv main.py homeaccountant
-chmod +x homeaccountant
-
